4e03762f130c427a8861128a69e2d01dd89c82f2
abigailcorona <abipalacios055@gmail.com>
2025-03-26 13:36:42 -0300

    x

--- TrabajoPractico_1/proyecto_1/main.py
+++ TrabajoPractico_1/proyecto_1/main.py
@@ -3,3 +3,17 @@ print("j")
 """hola"""
 
 """hola2"""
+
+"""PS C:\Users\alumno\Desktop\AyED2025c1-Micheloud-Corona-Lopez\TrabajoPractico_1\proyecto_1> git status"
+"PS C:\Users\alumno\Desktop\AyED2025c1-Micheloud-Corona-Lopez\TrabajoPractico_1\proyecto_1> git add .
+PS C:\Users\alumno\Desktop\AyED2025c1-Micheloud-Corona-Lopez\TrabajoPractico_1\proyecto_1> git status
+Changes to be committed:
+  (use "git restore --staged <file>..." to unstage)
+
+PS C:\Users\alumno\Desktop\AyED2025c1-Micheloud-Corona-Lopez\TrabajoPractico_1\proyecto_1> git commit -m "comentario inicial"
+PS C:\Users\alumno\Desktop\AyED2025c1-Micheloud-Corona-Lopez\TrabajoPractico_1\proyecto_1> git status
+On branch main
+Your branch is ahead of 'origin/main' by 1 commit.
+PS C:\Users\alumno\Desktop\AyED2025c1-Micheloud-Corona-Lopez\TrabajoPractico_1\proyecto_1> git push
+
+"""
