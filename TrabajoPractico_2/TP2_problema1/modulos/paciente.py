c97788a67ca294be833b70bd7ea6356e4b8d6f2b
natymicheloud <natymiche2@gmail.com>
2025-05-14 12:33:18 -0300

    cambios paciente

--- TrabajoPractico_2/TP2_problema1/modulos/paciente.py
+++ TrabajoPractico_2/TP2_problema1/modulos/paciente.py
@@ -8,13 +8,20 @@ descripciones_de_riesgo = ['crítico', 'moderado', 'bajo']
 # probabilidades de aparición de cada tipo de paciente
 probabilidades = [0.1, 0.3, 0.6] 
 
+# contador de llegada de pacientes
+contador_llegada=0
+
 class Paciente:
     def __init__(self):
+        global contador_llegada
         n = len(nombres)
         self.__nombre = nombres[randint(0, n-1)]
         self.__apellido = apellidos[randint(0, n-1)]
         self.__riesgo = choices(niveles_de_riesgo, probabilidades)[0]
         self.__descripcion = descripciones_de_riesgo[self.__riesgo-1]
+        self.__llegada = contador_llegada
+        contador_llegada += 1
+
 
     def get_nombre(self):
         return self.__nombre
@@ -33,3 +40,32 @@ class Paciente:
         cad += self.__apellido + '\t -> '
         cad += str(self.__riesgo) + '-' + self.__descripcion
         return cad
+    
+    # def __lt__(self, other):
+    #     if self.__riesgo > other.__riesgo:
+    #         turno= other.__riesgo
+
+    #     if self.__riesgo < other.__riesgo:
+    #         turno= self.__riesgo
+
+    #     else:
+    #         if self.__riesgo == other.__riesgo:
+    #             if self.__llegada > other.__llegada:
+    #                 turno = other.__llegada
+    #             else:
+    #                 turno= self.__llegada
+
+    #     return turno
+
+
+    def __lt__(self, other):
+        if self.__riesgo != other.__riesgo:
+            return self.__riesgo < other.__riesgo
+        return self.__llegada < other.__llegada
+
+Paciente1= Paciente()
+Paciente2= Paciente()
+if Paciente1 > Paciente2:
+    print("1")
+else:
+    print(Paciente1)
