39668c1bc4137c56e9c31091d15b2c53bdb7238b
abigailcorona <abipalacios055@gmail.com>
2025-05-14 13:09:57 -0300

    problema 1

--- TrabajoPractico_2/TP2_problema1/modulos/paciente.py
+++ TrabajoPractico_2/TP2_problema1/modulos/paciente.py
@@ -34,38 +34,29 @@ class Paciente:
     
     def get_descripcion_riesgo(self):
         return self.__descripcion
-    
-    def __str__(self):
-        cad = self.__nombre + ' '
-        cad += self.__apellido + '\t -> '
-        cad += str(self.__riesgo) + '-' + self.__descripcion
-        return cad
-    
-    # def __lt__(self, other):
-    #     if self.__riesgo > other.__riesgo:
-    #         turno= other.__riesgo
-
-    #     if self.__riesgo < other.__riesgo:
-    #         turno= self.__riesgo
-
-    #     else:
-    #         if self.__riesgo == other.__riesgo:
-    #             if self.__llegada > other.__llegada:
-    #                 turno = other.__llegada
-    #             else:
-    #                 turno= self.__llegada
-
-    #     return turno
-
 
     def __lt__(self, other):
         if self.__riesgo != other.__riesgo:
             return self.__riesgo < other.__riesgo
         return self.__llegada < other.__llegada
+    
+    def __str__(self):
+        cad = self.__nombre + ' '
+        cad += self.__apellido + '\t -> '
+        cad += str(self.__riesgo) + '-' + self.__descripcion + '-' + str(self.__llegada)
+        return cad
+    
+paciente1 = Paciente()
+paciente2 = Paciente()
+paciente3 = Paciente()
 
-Paciente1= Paciente()
-Paciente2= Paciente()
-if Paciente1 > Paciente2:
-    print("1")
-else:
-    print(Paciente1)
+print("Paciente 1:", paciente1)
+print("Paciente 2:", paciente2)
+print("Paciente 3:", paciente3)
+
+if paciente1 < paciente2 and paciente1 < paciente3:
+    print(f"{paciente1.get_nombre()} tiene mayor prioridad que {paciente2.get_nombre()} y {paciente3.get_nombre()}")
+elif paciente2 < paciente1 and paciente2 < paciente3:
+    print(f"{paciente2.get_nombre()} tiene mayor prioridad que {paciente1.get_nombre()} y {paciente3.get_nombre()}")
+elif paciente3 < paciente1 and paciente3 < paciente2:
+    print(f"{paciente3.get_nombre()} tiene mayor prioridad que {paciente1.get_nombre()} y {paciente2.get_nombre()}")
