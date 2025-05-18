d44ea58e2b0a2c5187bd6c9cf1f74061992a93b7
abigailcorona <abipalacios055@gmail.com>
2025-05-18 17:57:51 -0300

    1

--- TrabajoPractico_2/TP2_problema1/modulos/SalaEmergencia2.py
+++ TrabajoPractico_2/TP2_problema1/modulos/SalaEmergencia2.py
@@ -8,16 +8,15 @@ cola_de_espera = list()
 
 for n in range(20):
     paciente = pac.Paciente()
-    
     cola_de_espera.append(paciente)
 
 monticulo = mb.MonticuloBinario()
-monticulo.ConstruirMonticulo(cola_de_espera)
-print("Pacientes en la sala de espera:")
+
 for paciente in cola_de_espera:
+    monticulo.Insertar(paciente)
+    print("Pacientes en la sala de espera:")
     print(paciente)
 
-monticulo.Insertar(paciente)
 
 for paciente in cola_de_espera:
     if monticulo.tamañoactual > 0:
